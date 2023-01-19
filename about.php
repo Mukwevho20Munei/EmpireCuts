80c7474a481e0abed7e4076ade801bd2f8822848
Mukwevho20Munei <109991278+Mukwevho20Munei@users.noreply.github.com>
2023-01-19 02:24:23 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="container container-nav">
+            <div class="site-title">
+                <h1>Empire Cuts</h1>
+                <p class="subtitle">Welcome to Empire Cuts</p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="Homepage.php">Home</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="Members.php">Members</a></li>
+                    <li><a href="about.php" class="current-page">About</a></li>
+                    <li><a href="Booking.php">Booking</a></li>
+                    
+                </ul>
+
+            </nav>
+        </div>
+    </header>
+
+    <main class="about">
+        <div class="about-header">
+            <h1>About us</h1>
+        </div>
+        <div class="about-content">
+            <p>Empire cuts is the barber that is very unique and it takes a very good care of their customers and we make sure that we provide them with best service.
+                We are Located at Braamfontain The Full information about our Location is there under Details.You can even download our Application on play Store using this link <a href="about.html">www.EmpireCuts.co.za</a> And its free to download this App.
+            </p>
+            <p>we as EmpireCuts have decided to come up with an application and website that is going to help our customers to book their appointment online and also so that they can see our works and how we take care of our customers 
+                . We also show our customers our works on the Gallery so that they can see all hairstyles that we can do.</p>
+                    </div>
+                    <p><div class="flex-container">
+                        <div class="flex-item-right">
+                            <h4>We are located at</h4>
+                            <p>Braamfontain 132 Juta Street</p>
+                            <p>Gauteng, Johannesburg</p>
+                           
+                        </div>
+                        
+    </main>
+
+    <footer class="footer">
+        <h3><span id="demo">&copy;</span><a href="#">EmpireCuts.com</a></h3>
+        <h4>Phone</h4>
+                            <p>(+27)060-846-3443</p>
+                            <p>(+27)072-323-4354</p>
+                            <h4>Email</h4>
+                            <p>EmpireCuts@gmail.com</p>
+    </footer>
+
+    <script src="js/index.js"></script>
+</body>
+
+</html>
